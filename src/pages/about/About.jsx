50ca8984179591ccafccf378b671204f7d14f3ae
miecrose12 <itobiyeblossom75@gmail.com>
2024-07-29 14:54:51 +0100

    about homepage

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './About.css';
-
+import check from './assets/check.png'
+import check2 from './assets/check2.png'
 
 function App() {
   return (
@@ -12,21 +13,23 @@ function App() {
       </div>
       <div className="about">
         <div className="square">
-        <div className='circle'></div>
+          <img src={check} alt="" />
           <h2>Creating Opportunities</h2>
-          <p>Our software simplifies complex business processes, helping you efficiently manage tasks, projects, and resources.</p>
+          <p>Our software simplifies complex business processes, helping you efficiently manage tasks, projects, and resources. software simplifies complex business processes.</p>
         </div>
         <div className="square2">
-        <div className='circle2'></div>
+        <img src={check2} alt="" />
           <h2>Bridging the gap</h2>
           <p>Take a step back and imagine how much time it takes to find solutions to your “technical” problems and how much time it takes to connect to your “fix-man”. Here at SKILLEDman, we offer you a 2 in 1 solution.</p>
         </div>
         <div className="square3">
-        <div className='circle3'></div>
+        <img src={check} alt="" />
           <h2>Impeccable solutions</h2>
           <p>SKILLEDMan offers you<br/> time and resource for<br/> good and industry<br/> standard artisan solutions,<br/> Join us today and get<br/> minor problems done as<br/> soon as possible.</p>
         </div>
       </div>
+      
+      
     </div>
   );
 }
